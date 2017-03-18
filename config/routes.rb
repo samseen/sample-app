e5809e973b1637814c56acf6a98736bbe3966e20
Samson Akanbi <samson.akanbi@outlook.com>
2017-03-18 08:22:16 +0000

    Changed the landing page route to home

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
-  get 'foo/bar'
-
-  get 'foo/baz'
-
-  get 'static_pages/home'
-
   get 'static_pages/help'
   
   get 'static_pages/about'
   
   get 'static_pages/contact'
 
-  root 'application#hello'
+  root 'static_pages#home'
 end
