23fe0b911b9d47c88406eda19895ab2382bed058
Samson Akanbi <samson.akanbi@outlook.com>
2017-03-20 19:47:04 +0000

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -29,5 +29,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
+  
+  test "should get signup page" do
+    get signup_path
+    assert_response :success
+    assert_select "title", "Sign up | #{@base_title}"
+  end  
 
 end
