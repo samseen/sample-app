387130b9c1852f190ab4f77e29c617ce6a1d6e5e
Samson Akanbi <samson.akanbi@outlook.com>
2017-04-02 18:53:57 +0000

    Added a test to catch unwanted flash persistence. 'RED'

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,11 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by(email: params[:seesion][:email].downcase)
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
     else
-      # Create an error message.
+      flash[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
