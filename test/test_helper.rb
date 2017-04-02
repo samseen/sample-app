df4dc29f256d1bb2706c4fc6df9ad3c497628ca1
Samson Akanbi <samson.akanbi@outlook.com>
2017-04-02 22:47:00 +0000

    Implemented basic login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,7 +10,7 @@ class ActiveSupport::TestCase
   include ApplicationHelper
 
   # Returns true if a test user is logged in.
-  def is_logged_?
+  def is_logged_in?
     !session[:user_id].nil?
   end
 end
