9135e38ad64c4c5a936d1e3489e98004dac8c70e
Samson Akanbi <samson.akanbi@outlook.com>
2017-04-10 10:06:30 +0000

    Added the destroy action to the before_action filter

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -30,11 +30,20 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
   
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+                                    user: { password:              'foo',
+                                            password_confirmation: 'bar',
+                                            admin: true } }
+    assert_not @other_user.reload.admin?                                        
+  end
+  
   test "should redirect edit when logged is as wrong user" do
     log_in_as(@other_user)
     get edit_user_path(@user)
     assert flash.empty?
     assert_redirected_to root_url
   end
-  
 end
