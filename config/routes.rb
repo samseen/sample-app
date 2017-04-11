1ac75a085c1bf3747e74184c93cb75b1e0d14cb3
Samson Akanbi <samson.akanbi@outlook.com>
2017-04-11 12:22:10 +0000

    Added the account_activations route to only allow edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'  
+  root    'static_pages#home'  
   get     '/help',    to: 'static_pages#help'
   get     '/about',   to: 'static_pages#about'
   get     '/contact', to: 'static_pages#contact'
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get     '/login',   to: 'sessions#new'
   post    'login',    to: 'sessions#create'
   delete  '/logout',  to:   'sessions#destroy'
-  resources :users  
+  resources :users
+  resources :account_activations, only: [:edit]
 end
