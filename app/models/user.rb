5f13a1ac2597bcf5f102290669c440028872340c
Samson Akanbi <samson.akanbi@outlook.com>
2017-04-14 21:53:27 +0000

    updated the addition of password reset methods to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,14 +44,26 @@ class User < ApplicationRecord
   
   # Activates an account.
   def activate
-    # update_attribute(:activated,    true)
-    # update_attribute(:activated_at, Time.zone.now)
-    update_columns(activated: true, activated_at: Time.zone.now)
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+    # update_columns(activated: true, activated_at: Time.zone.now)
   end
   
   # Sends activation email.
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+  
+  # Sets the password reset attributes.
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,   User.digest(reset_token))
+    update_attribute(:reset_sent_at,  Time.zone.now)
+  end
+  
+  # Sends password reset email.
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
   end
   
   private
